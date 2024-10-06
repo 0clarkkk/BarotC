a88287e49e3f27dfd7c1b01ef9dca53fcf7281df
0clarkkk <anchetac300@gmail.com>
2024-10-06 14:37:08 +0800

    first-commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = "app/home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "app/about.html"
+
+class PortfolioPageView(TemplateView):
+    template_name = "app/portfolio.html"    
+
+class ContactPageView(TemplateView):
+    template_name = "app/contact.html"  
